83e16fcfb8480f00da4fc2a73e340959dc4a919b
Tergel Munkhdelger <tergelm@gmail.com>
2021-01-28 22:47:11 +0800

    animations

--- components/HomeComponents/Hero.tsx
+++ components/HomeComponents/Hero.tsx
@@ -1,13 +1,37 @@
 import { Box, Heading } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Container } from "../common";
 
-const Hero: React.FC<{}> = () => {
+const TitleVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
+const Hero: React.FC<{ active: boolean }> = ({ active }) => {
   return (
     <div className="section">
       <Container>
-        <Heading as="h1" size="4xl">
-          TERGEL MUNKHDELGER
-        </Heading>
+        <AnimatePresence>
+          {active && (
+            <Heading as="h1" size="4xl">
+              <motion.div
+                variants={TitleVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                TERGEL MUNKHDELGER
+              </motion.div>
+            </Heading>
+          )}
+        </AnimatePresence>
       </Container>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
 import { Layout, About, Contact, Hero, Projects } from "../components";
 import ReactFullpage from "@fullpage/react-fullpage";
+import { useState } from "react";
 
 const anchors = ["top", "about", "projects", "contact"];
 
 const Home = () => {
+  const [activeStep, setActiveStep] = useState<number | null>(null);
+
   return (
     <Layout>
       <ReactFullpage
         navigation
         navigationPosition="left"
         scrollingSpeed="1000"
+        onLeave={() => setActiveStep(null)}
+        afterLoad={(origin, destination, direction) =>
+          setActiveStep(destination.index)
+        }
         anchors={anchors}
         render={() => {
           return (
             <ReactFullpage.Wrapper>
-              <Hero />
+              <Hero active={activeStep === 0} />
               <About />
               <Projects />
               <Contact />
