76fd77f93031735a2fd3c9a4ea9090265d25a3a4
mtergel <tergelm@gmail.com>
2021-04-05 20:38:16 +0800

    init path

---
+++ pages/projects/[projectName].tsx
@@ -0,0 +1,15 @@
+import path from "path";
+import fs from "fs";
+
+const SingleProjectPage = () => {
+
+  return <div>HI</div>;
+};
+
+export async function getStaticProps() {
+
+    const md = 
+
+}
+
+export default SingleProjectPage;
---
+++ pages/projects/index.tsx
@@ -0,0 +1,74 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  Icon,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import { Layout } from "../../components";
+import { Container } from "../../components/common";
+import { ImFilesEmpty } from "react-icons/im";
+import NextLink from "next/link";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { getSortedProjectsData } from "../../lib/projects";
+
+const Projects = () => {
+  return (
+    <Layout>
+      <Container paddingTop>
+        <Box
+          width="100%"
+          display="flex"
+          alignItems="center"
+          marginBottom="40px"
+        >
+          <Heading flexShrink={0}>Projects</Heading>
+          <Divider marginLeft="40px" />
+        </Box>
+        <Box
+          width="100%"
+          height="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDir="column"
+        >
+          <Icon as={ImFilesEmpty} fontSize="2xl" />
+          <Heading size="md" marginTop="16px" marginBottom="48px">
+            It's bit empty here...
+          </Heading>
+          <Box display="flex" alignItems="center">
+            <Text>
+              Check out my
+              <NextLink href="https://github.com/mtergel" passHref>
+                <Button mx={3}>Github</Button>
+              </NextLink>
+              for now.
+            </Text>
+          </Box>
+          <Box marginY={"4rem"}>
+            <NextLink href="/" passHref>
+              <Link width="100%">
+                <Icon as={BsArrowLeftShort} mr={4} fontSize="lg" />
+                Back
+              </Link>
+            </NextLink>
+          </Box>
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Projects;
+
+export async function getStaticProps() {
+  const allProjectsData = getSortedProjectsData();
+  return {
+    props: {
+      allProjectsData,
+    },
+  };
+}
